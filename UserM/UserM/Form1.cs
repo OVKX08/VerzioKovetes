2891a43ad5fd74da3a80bd03141d3098e0fed443
Máté Nagy <mate.nagy6@stud.uni-corvinus.hu>
2022-10-03 22:49:34 +0200

    users added

--- UserM/UserM/Form1.cs
+++ UserM/UserM/Form1.cs
@@ -7,17 +7,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserM.Entities;
 
 namespace UserM
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
             label1.Text = Resource.LastName; //label1
             label2.Text = Resource.FirstName; //labe2
             button1.Text = Resource.Add;
+
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+
+            var u = new User()
+            {
+                LastName = textBox1.Text,
+                FirstName = textBox2.Text
+            };
+            users.Add(u);
         }
 
         private void Form1_Load(object sender, EventArgs e)
