5a29632dd90ba3a1419f4784b81d725ed5187525
Máté Nagy <mate.nagy6@stud.uni-corvinus.hu>
2022-10-06 17:46:53 +0200

    otodik

--- negyedik/negyedik/Form1.cs
+++ negyedik/negyedik/Form1.cs
@@ -7,7 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 namespace negyedik
 {
     public partial class Form1 : Form
