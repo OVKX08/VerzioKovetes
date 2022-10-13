fba151061bb62221aa56401cf9a234647d27f9f0
Máté Nagy <mate.nagy6@stud.uni-corvinus.hu>
2022-10-13 14:09:08 +0200

    loaddone

--- gyakorlo/gyakorlo/Form1.cs
+++ gyakorlo/gyakorlo/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,13 +13,48 @@ namespace gyakorlo
 {
     public partial class Form1 : Form
     {
+        List<OlympicResult> results;
+
+
         public Form1()
         {
             InitializeComponent();
+            LoadData("Summer_olympic_Medals.csv");
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
+        }
+
+        private void LoadData(string filename)
+        {
+            StreamReader sr = new StreamReader(filename, Encoding.Default);
+
+            var header = sr.ReadLine().Split(',');
+
+            
+
+            while (!sr.EndOfStream)
+            {
+                OlympicResult temp = new OlympicResult();
+                
+                string[] line = sr.ReadLine().Split(',');
+                
+                temp.Year = int.Parse(line[0]);
+                temp.Country = line[3];
+                temp.Medals[0] = int.Parse(line[5]);
+                temp.Medals[1] = int.Parse(line[6]);
+                temp.Medals[2] = int.Parse(line[7]);
+                
+                results.Add(temp);
+                
+
+
+                
+
+            }
+
 
         }
     }
--- gyakorlo/gyakorlo/folder1/OlympicResult.cs
+++ gyakorlo/gyakorlo/folder1/OlympicResult.cs
@@ -10,7 +10,7 @@ namespace gyakorlo
     {
         public int Year { get; set; }
         public string Country { get; set; }
-        public int Medals { get; set; }
+        public int[] Medals { get; set; }
         public int Position { get; set; }
 
     }
