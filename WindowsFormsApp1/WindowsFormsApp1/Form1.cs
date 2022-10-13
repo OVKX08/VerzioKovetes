5a89f6c3932f8ab57eb9588c3ef4d58fcb9e826c
Máté Nagy <mate.nagy6@stud.uni-corvinus.hu>
2022-10-13 11:26:57 +0200

    hupszi

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -7,14 +7,108 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        List<Flat> flats;
+        RealEstateEntities context = new RealEstateEntities();
+
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoadData()
+        {
+            flats = context.Flats.ToList();
+        }
+
+        private void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
+
+                CreateTable();
+
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+                
+            }
+
+        }
+
+        private void CreateTable()
+        {
+            string[] headers =
+            {
+                "Kód",
+                "Eladó",
+                "Oldal",
+                "Kerület",
+                "Lift",
+                "Szobák száma",
+                "Alapterulet (m2)",
+                "Ár (mFt)",
+                "Négyzetméter ár (Ft/m2)"
+            };
+
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, i + 1] = headers[i];
+               
+            }
+
+            object[,] values = new object[flats.Count, headers.Length];
+
+            int counter = 0;
+            foreach (Flat f in flats)
+            {
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
+                if (f.Elevator) values[counter, 4] = "Van";
+                    else values[counter, 4] = "Nincs";
+                values[counter, 5] = f.NumberOfRooms;
+                values[counter, 6] = f.FloorArea;
+                values[counter, 7] = f.Price;
+                values[counter, 8] = $"=(H{counter+2}/ G{counter+2})*1000000";
+
+                counter++;
+            } 
+        }
+
+        private void GetCell(int x, int y)
+        {
+
         }
     }
 }
