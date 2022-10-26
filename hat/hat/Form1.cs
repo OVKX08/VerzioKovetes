d46755ea4d15038aa0c79456c118f9672bf1511b
Máté Nagy <mate.nagy6@stud.uni-corvinus.hu>
2022-10-26 20:17:35 +0200

    conveyortimer

--- hat/hat/Form1.cs
+++ hat/hat/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using hat.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,39 @@ namespace hat
 {
     public partial class Form1 : Form
     {
+        private List<Ball> _balls = new List<Ball>();
+
+        private BallFactory _factory;
+        public BallFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
+
         public Form1()
         {
             InitializeComponent();
+            Factory = new BallFactory();
+        }
+
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            var ball = Factory.CreateNew();
+            _balls.Add(ball);
+            ball.Left = -ball.Width;
+
+            mainPanel.Controls.Add(ball);
         }
     }
 }
