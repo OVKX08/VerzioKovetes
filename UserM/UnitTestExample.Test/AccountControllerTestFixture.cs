a3719e704a296f7e04b52a2c7ef9ef9c73129b27
Máté Nagy <mate.nagy6@stud.uni-corvinus.hu>
2022-11-25 12:22:03 +0100

    email vali

---
+++ UserM/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestExample.Controllers;
+
+namespace UnitTestExample.Test
+{
+    public class AccountControllerTestFixture
+    {
+
+        [
+            Test,
+            TestCase("Password1",false),
+            TestCase("mate1@gmail", false),
+            TestCase("mate1gmail.com", false),
+            TestCase("mate1.gmail.com",false),
+            TestCase("mate1@gmail.com", true)
+        ]
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+            //Arrange
+            var accountController = new AccountController();
+
+            //Act
+            var actualResult = accountController.ValidateEmail(email);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+
+
+        
+    }
+}
