1be5e862ab740bc90f1f29b37fd9459d657ef053
Máté Nagy <mate.nagy6@stud.uni-corvinus.hu>
2022-12-07 21:15:49 +0100

    4

--- week10/week10/Form1.cs
+++ week10/week10/Form1.cs
@@ -21,6 +21,8 @@ namespace week10
         int nbrOfStepsIncrement = 10;
         int generation = 1;
 
+        Brain winnerBrain = null;
+
         public Form1()
         {
             InitializeComponent();
@@ -76,6 +78,17 @@ namespace week10
                     gc.AddPlayer(b.Mutate());
             }
             gc.Start();
+
+            var winners = from p in topPerformers
+                          where p.IsWinner
+                          select p;
+
+            if (winners.Count()>0)
+            {
+                winnerBrain = winners.FirstOrDefault().Brain.Clone();
+                gc.GameOver -= Gc_GameOver;
+                return;
+            }
 
         }
 
